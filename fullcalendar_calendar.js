b0096bc1c13fafc91ecaa0f37c3aaa49e381137c
Wes Jones <waj194@gmail.com>
2015-05-04 10:50:28 -0400

    Build ajax save callback for eventDrop.

--- fullcalendar_calendar.js
+++ fullcalendar_calendar.js
@@ -4,15 +4,51 @@
  */
 
 (function($) {
-  Drupal.behaviors.fullCalendarApiDemo = {
+  Drupal.behaviors.fullCalendarApiCalendar = {
     attach: function(context, settings) {
 
       var $calendar = $('#fullcalendar');
       if (!$calendar.length || !settings.fullcalendar_api.calendarSettings) {
         return;
       }
-      $calendar.fullCalendar(settings.fullcalendar_api.calendarSettings);
+      var $settings = settings.fullcalendar_api.calendarSettings;
+      // Merge in event callbacks.
+      $.extend($settings, {
+        eventDrop: function(event, delta, revertFunc) {
+
+          // Create a simple object to send to ajax function.
+          var $data = {
+            id: event.id,
+            title: event.title,
+            entityType: event.entityType ? event.entityType : null,
+            bundle: event.bundle ? event.bundle : null,
+            allDay: event.allDay,
+            dateField: event.dateField,
+            startTime: event.start.unix(),
+            endTime: event.end ? event.end.unix() : null
+          };
+
+          if (!confirm("Are you sure about this change?")) {
+            revertFunc();
+          }
+          else {
+            // Save the entity via AJAX.
+            $.ajax({
+              method: 'POST',
+              url: '//' + window.location.hostname +  settings.basePath + settings.pathPrefix + 'fullcalendar-api/ajax/update/drop/' + event.id,
+              data: $data
+            }).done(function(response){
+              // @todo return success?
+              if (response == 'failure') {
+                revertFunc();
+              }
+            });
+          }
+
+        }
+      });
       
+      $calendar.fullCalendar($settings);
     } 
   }
 })(jQuery);
